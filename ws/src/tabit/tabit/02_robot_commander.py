449bcecd609b91fbfe8e76f5f1fdd17ab87523fd
Hassan Umari <hassan.umari@tabit-gmbh.de>
2025-09-21 13:55:03 +0200

    adapt robot commander

--- ws/src/tabit/tabit/02_robot_commander.py
+++ ws/src/tabit/tabit/02_robot_commander.py
@@ -1,21 +1,21 @@
 import rclpy
 from rclpy.node import Node
-from geometry_msgs.msg import TwistStamped
+from geometry_msgs.msg import Twist
 
 
 class RobotCommander(Node):
     def __init__(self):
         super().__init__("robot_commander")
-        self.publisher = self.create_publisher(TwistStamped, "/cmd_vel", 10)
+        self.publisher = self.create_publisher(Twist, "/cmd_vel", 10)
         self.timer = self.create_timer(0.1, self.timer_callback)
-        self.msg = TwistStamped()
+        self.msg = Twist()
 
     def timer_callback(self):
         # let the robot move in a circle with a radius of 50 cm, at a speed of 30 cm/sec
         radius = 0.5  # m
         velocity = 0.3  # m/s
-        self.msg.twist.linear.x = velocity
-        self.msg.twist.angular.z = velocity / radius
+        self.msg.linear.x = velocity
+        self.msg.angular.z = velocity / radius
         self.publisher.publish(self.msg)
 
 
